7edc78981ec5d60ff13beb6909414ec162bf62f0
zimmmvp <zimmdq.gaming@gmail.com>
2026-07-14 20:58:08 -0300

    Update app.js

--- app.js
+++ app.js
@@ -9,3 +9,16 @@ async function iniciarPlanner() {
     // para usarlos cuando el usuario haga click en un item
     console.log("Datos cargados correctamente");
 }
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Planner cargado. Esperando archivos de datos...");
+    
+    // Aquí cargaremos los txt más adelante:
+    // fetch('item.txt').then(...)
+    // fetch('stats.txt').then(...)
+});
+
+// Función para cuando el usuario seleccione un item
+function equiparItem(item) {
+    console.log("Equipando:", item.nombre);
+    // Aquí irá la lógica para actualizar el 'slot' correspondiente
+}
