514a51ca5f3387ec3e49429b45794a8bc4d5be22
zimmmvp <zimmdq.gaming@gmail.com>
2026-07-14 20:12:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 
+const habilidades = [
+    { nombre: "Golpe de Sombras", nivel: 0 },
+    { nombre: "Escudo de Embolia", nivel: 0 }
+];
+
+function renderSkills() {
+    const list = document.getElementById('skills-list');
+    list.innerHTML = habilidades.map(s => `
+        <div class="skill-item">
+            ${s.nombre} - Puntos: ${s.nivel}
+        </div>
+    `).join('');
+}
+
+// Inicializar
+renderSkills();
