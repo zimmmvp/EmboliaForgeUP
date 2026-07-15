26d3890750216951bdfc923ba79d1d7536043ca4
zimmmvp <zimmdq.gaming@gmail.com>
2026-07-15 20:49:36 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+const LISTA_STATS = [
+    "Vida Maxima", "Mana Maxima", "Ataque", "Ataque Magico", "Defensa", "Defensa Magica", 
+    "Evasion", "Resistencia al fuego", "Resistencia a los rayos", "Resistencia al frio",
+    "Dano de Fuego", "Dano de Rayo", "Dano de Frio", "Critico", "Critico Magico", 
+    "Poder Critico", "Poder Critico Magico", "Velocidad de Movimiento", "Regeneracion Vida", 
+    "Regeneracion Mana", "Chance de Bloqueo", "Bloqueo de Dano", "Experiencia aumentada obtenida",
+    "Ganancia de Vida", "Area de Efecto", "Hallazgo magico", "Velocidad de mineria", 
+    "Eficiencia minera", "Cantidad de mineria", "Raridad minera", "Generador de mineria", 
+    "Radio de agarre minero"
+];
