2342572c6905e148a232601972085b9174c70dea
zimmmvp <zimmdq.gaming@gmail.com>
2026-07-14 22:17:34 -0300

    Create items_data.js

---
+++ items_data.js
@@ -0,0 +1,11 @@
+const baseItems = {
+    "armadura": [
+        { id: 100, nombre: "Armadura Ligera de Doom", defensaBase: 100 },
+        { id: 101, nombre: "Túnica de Sombras", defensaBase: 50 }
+    ]
+};
+
+const modificadores = [
+    { nombre: "Resistencia Fuego", rango: "5-30%" },
+    { nombre: "Vida", rango: "10-50" }
+];
