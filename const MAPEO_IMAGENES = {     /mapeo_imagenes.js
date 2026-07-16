29a787fdada105d1ebbf1c094568506fb19a86c1
zimmmvp <zimmdq.gaming@gmail.com>
2026-07-16 20:24:01 -0300

    Delete const MAPEO_IMAGENES = {      directory

--- const MAPEO_IMAGENES = {     /mapeo_imagenes.js
+++
@@ -1,6 +0,0 @@
-const MAPEO_IMAGENES = {
-    // ID del ítem : "Nombre exacto de tu archivo en la carpeta img"
-    "item_1": "IMG_001.png",
-    "item_2": "cuchillo_viejo_final.png",
-    "item_3": "espada_de_prueba_v2.jpg"
-};
